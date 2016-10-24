35b502ba84fe03efbc33d088e4123ae17ea0d9f3
ajlopez <webmaster@ajlopez.com>
2016-10-24 14:23:14 -0300

    BlockData

---
+++ Src/PegSharp.Tests/External/BlockDataTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PegSharp.Tests.External
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PegSharp.External;
+
+    [TestClass]
+    public class BlockDataTests
+    {
+        [TestMethod]
+        public void CreateWithNumberHash()
+        {
+            var block = new BlockData();
+
+            Assert.AreEqual(0, block.Number);
+            Assert.IsNotNull(block.Hash);
+            Assert.IsNotNull(block.Hash.Bytes);
+            Assert.AreEqual(32, block.Hash.Bytes.Length);
+        }
+    }
+}
---
+++ Src/PegSharp/External/BlockData.cs
@@ -0,0 +1,22 @@
+﻿namespace PegSharp.External
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockData
+    {
+        private long number;
+        private Hash hash;
+
+        public BlockData()
+        {
+            this.hash = new Hash();
+        }
+
+        public long Number { get { return this.number; } }
+
+        public Hash Hash { get { return this.hash; } }
+    }
+}
