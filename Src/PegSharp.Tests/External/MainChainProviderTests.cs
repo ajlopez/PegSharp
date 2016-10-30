b2365695269f8cbf0f0fae14808573cb80e5a69e
ajlopez <webmaster@ajlopez.com>
2016-10-30 11:18:53 -0300

    Empty MainChainProvider

---
+++ Src/PegSharp.Tests/External/MainChainProviderTests.cs
@@ -0,0 +1,18 @@
+﻿namespace PegSharp.Tests.External
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PegSharp.External;
+
+    [TestClass]
+    public class MainChainProviderTests
+    {
+        [TestMethod]
+        public void EmptyMainChain()
+        {
+            var chain = new MainChainProvider();
+
+            Assert.AreEqual(0, chain.Height);
+        }
+    }
+}
---
+++ Src/PegSharp/External/MainChainProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace PegSharp.External
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class MainChainProvider
+    {
+        public long Height { get { return 0; } }
+    }
+}
