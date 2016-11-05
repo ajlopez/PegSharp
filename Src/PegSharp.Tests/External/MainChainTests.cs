ebcfd7b613ccc416ef10228aea214f2b804d5800
ajlopez <webmaster@ajlopez.com>
2016-11-05 14:59:39 -0300

    Empty MainChain

---
+++ Src/PegSharp.Tests/External/MainChainTests.cs
@@ -0,0 +1,18 @@
+﻿namespace PegSharp.Tests.External
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PegSharp.External;
+
+    [TestClass]
+    public class MainChainTests
+    {
+        [TestMethod]
+        public void CreateEmptyMainChain()
+        {
+            var chain = new MainChain();
+
+            Assert.AreEqual(0, chain.Height);
+        }
+    }
+}
