019fb394041854c0bfc5160bdcbf0453b2a9973e
ajlopez <webmaster@ajlopez.com>
2016-11-08 13:45:31 -0300

    Additional test, add block

--- Src/PegSharp.Tests/External/BlockChainTests.cs
+++ Src/PegSharp.Tests/External/BlockChainTests.cs
@@ -26,5 +26,19 @@
             Assert.AreEqual(1, chain.Height);
             Assert.AreSame(genesis, chain.BestBlock);
         }
+
+        [TestMethod]
+        public void AddGenesisAndBlock()
+        {
+            var chain = new BlockChain();
+            var genesis = new BlockData(0);
+            var block = new BlockData(genesis);
+
+            chain.Add(genesis);
+            chain.Add(block);
+
+            Assert.AreEqual(2, chain.Height);
+            Assert.AreSame(block, chain.BestBlock);
+        }
     }
 }
