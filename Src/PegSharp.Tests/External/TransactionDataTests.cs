0cb28be858cbeaa2b35f8160bc9539a9a35d7862
ajlopez <webmaster@ajlopez.com>
2016-11-20 15:41:57 -0300

    TransactionData first simple create test

---
+++ Src/PegSharp.Tests/External/TransactionDataTests.cs
@@ -0,0 +1,18 @@
+﻿namespace PegSharp.Tests.External
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PegSharp.External;
+
+    [TestClass]
+    public class TransactionDataTests
+    {
+        [TestMethod]
+        public void CreateWithHash()
+        {
+            var data = new TransactionData();
+
+            Assert.IsNotNull(data.Hash);
+        }
+    }
+}
---
+++ Src/PegSharp/External/TransactionData.cs
@@ -0,0 +1,19 @@
+﻿namespace PegSharp.External
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TransactionData
+    {
+        private Hash hash;
+
+        public TransactionData()
+        {
+            this.hash = new Hash();
+        }
+
+        public Hash Hash { get { return this.hash; } }
+    }
+}
