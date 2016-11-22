c121e362ef6b2a3368a3ddc77970b55da4409791
ajlopez <webmaster@ajlopez.com>
2016-11-22 18:21:15 -0300

    TransactionData equals using hash

--- Src/PegSharp.Tests/External/TransactionDataTests.cs
+++ Src/PegSharp.Tests/External/TransactionDataTests.cs
@@ -14,5 +14,21 @@
 
             Assert.IsNotNull(data.Hash);
         }
+
+        [TestMethod]
+        public void Equals()
+        {
+            var tx1 = new TransactionData();
+            var tx2 = new TransactionData(tx1.Hash);
+            var tx3 = new TransactionData();
+
+            Assert.AreEqual(tx1, tx2);
+            Assert.AreEqual(tx2, tx1);
+            Assert.AreNotEqual(tx1, tx3);
+            Assert.AreNotEqual(tx1, 42);
+            Assert.AreNotEqual(tx1, "foo");
+
+            Assert.AreEqual(tx1.GetHashCode(), tx2.GetHashCode());
+        }
     }
 }
--- Src/PegSharp/External/TransactionData.cs
+++ Src/PegSharp/External/TransactionData.cs
@@ -14,6 +14,27 @@
             this.hash = new Hash();
         }
 
+        public TransactionData(Hash hash)
+        {
+            this.hash = hash;
+        }
+
         public Hash Hash { get { return this.hash; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is TransactionData))
+                return false;
+
+            return this.hash.Equals(((TransactionData)obj).hash);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.hash.GetHashCode();
+        }
     }
 }
